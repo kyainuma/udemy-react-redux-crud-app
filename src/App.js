12ee2d0304db879fbbb38252d086bbcbaa7ce0d7
kyainuma <yasu6491@icloud.com>
2022-12-30 11:10:07 +0900

    Create function component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { render } from "@testing-library/react";
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {alert("clicked!")}} />
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {alert("clicked!")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
